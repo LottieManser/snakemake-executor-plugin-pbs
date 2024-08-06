68ae12c6224c05ba19c9c27da696abcf0b313205
Daniel O'Hanlon <dohanlon@ebi.ac.uk>
2024-08-06 01:24:28 +0100

    Add a hacky implementation that works around not having statuses available when a job completes.

--- snakemake_executor_plugin_pbs/__init__.py
+++ snakemake_executor_plugin_pbs/__init__.py
@@ -1,3 +1,6 @@
+import os
+import stat
+
 from dataclasses import dataclass, field
 from typing import List, Generator, Optional
 from snakemake_interface_executor_plugins.executors.base import SubmittedJobInfo
@@ -6,138 +9,144 @@ from snakemake_interface_executor_plugins.settings import (
     ExecutorSettingsBase,
     CommonSettings,
 )
-from snakemake_interface_executor_plugins.jobs import (
-    JobExecutorInterface,
-)
+from snakemake_interface_executor_plugins.jobs import JobExecutorInterface
 from snakemake_interface_common.exceptions import WorkflowError  # noqa
+import subprocess
 
+def writePBSScript(name, resources, command):
 
-# Optional:
-# Define additional settings for your executor.
-# They will occur in the Snakemake CLI as --<executor-name>-<param-name>
-# Omit this class if you don't need any.
-# Make sure that all defined fields are Optional and specify a default value
-# of None or anything else that makes sense in your case.
+    home = os.path.expanduser("~")
+    location = home + '/jobs/' + name + '_job.sh'
+
+    script = r'#!/bin/bash' + '\n'
+    script += f'#PBS -o {home}/logs/'+ name + '_out.log' + '\n'
+    script += f'#PBS -e {home}/logs/'+ name + '_err.log' + '\n'
+    script += r'#PBS -l walltime=' + str(resources.walltime) + '\n'
+    script += r'#PBS -lselect=1:ncpus='+ str(resources.cpus) + ':mem=' + str(resources.mem) + 'gb' + '\n'
+    script += r'#PBS -N ' + name + '\n'
+    script += r'#PBS -V' + '\n'
+    script += r'cd $PBS_O_WORKDIR;' + '\n'
+    script += 'time ' + command
+
+    f = open(location, 'w')
+    f.write(script)
+    f.close()
+
+    return location
+
+def writeBashScript(location, name, command):
+
+    fileName = location + '/' + name + '.sh'
+
+    f = open(fileName, 'w')
+    f.write(r'#!/bin/bash' + '\n')
+    f.write(f'cd {os.getcwd()};' + '\n')
+    f.write(command + '\n')
+    f.write('echo "exit status: $?"' +'\n') # Exit status for bookkeeping
+    f.close()
+
+    # Make the file executable
+    subprocess.call(['chmod', '+x', fileName])
+
+    return fileName
+
+def lastline(filename):
+    # Assuming filename exists
+    return subprocess.check_output(['tail', '-1', filename])
+
 @dataclass
 class ExecutorSettings(ExecutorSettingsBase):
-    myparam: Optional[int] = field(
+    # Example of a custom setting parameter for PBS
+    pbs_queue: Optional[str] = field(
         default=None,
         metadata={
-            "help": "Some help text",
-            # Optionally request that setting is also available for specification
-            # via an environment variable. The variable will be named automatically as
-            # SNAKEMAKE_<executor-name>_<param-name>, all upper case.
-            # This mechanism should only be used for passwords and usernames.
-            # For other items, we rather recommend to let people use a profile
-            # for setting defaults
-            # (https://snakemake.readthedocs.io/en/stable/executing/cli.html#profiles).
+            "help": "Queue name for PBS jobs",
             "env_var": False,
-            # Optionally specify a function that parses the value given by the user.
-            # This is useful to create complex types from the user input.
-            "parse_func": ...,
-            # If a parse_func is specified, you also have to specify an unparse_func
-            # that converts the parsed value back to a string.
-            "unparse_func": ...,
-            # Optionally specify that setting is required when the executor is in use.
-            "required": True,
+            "required": False,
         },
     )
 
 
-# Required:
-# Specify common settings shared by various executors.
 common_settings = CommonSettings(
-    # define whether your executor plugin executes locally
-    # or remotely. In virtually all cases, it will be remote execution
-    # (cluster, cloud, etc.). Only Snakemake's standard execution
-    # plugins (snakemake-executor-plugin-dryrun, snakemake-executor-plugin-local)
-    # are expected to specify False here.
     non_local_exec=True,
-    # Whether the executor implies to not have a shared file system
-    implies_no_shared_fs=True,
-    # whether to deploy workflow sources to default storage provider before execution
+    implies_no_shared_fs=False,
     job_deploy_sources=True,
-    # whether arguments for setting the storage provider shall be passed to jobs
     pass_default_storage_provider_args=True,
-    # whether arguments for setting default resources shall be passed to jobs
     pass_default_resources_args=True,
-    # whether environment variables shall be passed to jobs (if False, use
-    # self.envvars() to obtain a dict of environment variables and their values
-    # and pass them e.g. as secrets to the execution backend)
     pass_envvar_declarations_to_cmd=True,
-    # whether the default storage provider shall be deployed before the job is run on
-    # the remote node. Usually set to True if the executor does not assume a shared fs
     auto_deploy_default_storage_provider=True,
-    # specify initial amount of seconds to sleep before checking for job status
     init_seconds_before_status_checks=0,
 )
 
 
-# Required:
-# Implementation of your executor
 class Executor(RemoteExecutor):
     def __post_init__(self):
-        # access workflow
         self.workflow
-        # access executor specific settings
         self.workflow.executor_settings
 
-        # IMPORTANT: in your plugin, only access methods and properties of
-        # Snakemake objects (like Workflow, Persistence, etc.) that are
-        # defined in the interfaces found in the
-        # snakemake-interface-executor-plugins and the
-        # snakemake-interface-common package.
-        # Other parts of those objects are NOT guaranteed to remain
-        # stable across new releases.
-
-        # To ensure that the used interfaces are not changing, you should
-        # depend on these packages as >=a.b.c,<d with d=a+1 (i.e. pin the
-        # dependency on this package to be at least the version at time
-        # of development and less than the next major version which would
-        # introduce breaking changes).
-
-        # In case of errors outside of jobs, please raise a WorkflowError
-
     def run_job(self, job: JobExecutorInterface):
-        # Implement here how to run a job.
-        # You can access the job's resources, etc.
-        # via the job object.
-        # After submitting the job, you have to call
-        # self.report_job_submission(job_info).
-        # with job_info being of type
-        # snakemake_interface_executor_plugins.executors.base.SubmittedJobInfo.
-        # If required, make sure to pass the job's id to the job_info object, as keyword
-        # argument 'external_job_id'.
+        # Prepare the job submission command
+        job_cmd = self.format_job_exec(job)
 
-        ...
+        home = os.path.expanduser("~")
+
+        if not os.path.exists(home + '/logs'):
+            os.makedirs(home + '/logs')
+            os.makedirs(home + '/jobs')
+
+        name = f'{job.rule.name}.{job.jobid}'
+
+        bashLoc = writeBashScript(home + '/jobs', name, job_cmd)
+        scriptLoc = writePBSScript(name, job.resources, bashLoc)
+
+        cmd = f"qsub {scriptLoc}"
+
+        try:
+            # Submit the job to PBS
+            result = subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
+            job_id = result.stdout.strip()
+            job_info = SubmittedJobInfo(job=job, external_jobid=job_id)
+            self.report_job_submission(job_info)
+        except subprocess.CalledProcessError as e:
+            raise WorkflowError(f"Failed to submit job: {e.stderr}")
 
     async def check_active_jobs(
         self, active_jobs: List[SubmittedJobInfo]
     ) -> Generator[SubmittedJobInfo, None, None]:
-        # Check the status of active jobs.
+        async with self.status_rate_limiter:
+            for job in active_jobs:
+                cmd = f"qstat {job.external_jobid}"
+                result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
 
-        # You have to iterate over the given list active_jobs.
-        # If you provided it above, each will have its external_jobid set according
-        # to the information you provided at submission time.
-        # For jobs that have finished successfully, you have to call
-        # self.report_job_success(active_job).
-        # For jobs that have errored, you have to call
-        # self.report_job_error(active_job).
-        # This will also take care of providing a proper error message.
-        # Usually there is no need to perform additional logging here.
-        # Jobs that are still running have to be yielded.
-        #
-        # For queries to the remote middleware, please use
-        # self.status_rate_limiter like this:
-        #
-        # async with self.status_rate_limiter:
-        #    # query remote middleware here
-        #
-        # To modify the time until the next call of this method,
-        # you can set self.next_sleep_seconds here.
-        ...
+                name = f'{job.job.rule.name}.{job.job.jobid}'
+
+                if result.returncode != 0:  # Job is not found
+
+                    home = os.path.expanduser("~")
+
+                    logFile = f"{home}/logs/{name}_out.log"
+
+                    # Check the logs to get the exit status
+                    # (This might be a race condition if the job finishes between the qstat and the check of the log file)
+                    if os.path.exists(logFile):
+                        statusline = lastline(logFile)
+                        exit_status = int(statusline.split()[-1])
+
+                        if exit_status != 0:
+                            self.report_job_error(job)
+                            continue
+                        else:
+                            self.report_job_success(job)
+                            continue
+
+                    self.report_job_error(job)
+                elif " C " in result.stdout:  # PBS marks completed jobs with a " C "
+                    self.report_job_success(job)
+                else:
+                    # Still running
+                    yield job
 
     def cancel_jobs(self, active_jobs: List[SubmittedJobInfo]):
-        # Cancel all active jobs.
-        # This method is called when Snakemake is interrupted.
-        ...
+        for job in active_jobs:
+            cmd = f"qdel {job.external_jobid}"
+            subprocess.run(cmd, shell=True, capture_output=True, text=True)
