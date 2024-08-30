cf807a96b0897b9e0756e57ef10fa96cf284d83a
Daniel O'Hanlon <dpohanlon@gmail.com>
2024-08-30 17:38:11 +0100

    Add code to combine jobs with the same rule name into an array job.

--- snakemake_executor_plugin_pbs/__init__.py
+++ snakemake_executor_plugin_pbs/__init__.py
@@ -13,8 +13,7 @@ from snakemake_interface_common.exceptions import WorkflowError  # noqa
 import subprocess
 
 
-def writePBSScript(name, resources, command):
-
+def writePBSScript(name, resources, array_indices, command):
     home = os.path.expanduser("~")
     location = home + "/jobs/" + name + "_job.sh"
 
@@ -26,8 +25,6 @@ def writePBSScript(name, resources, command):
     if hasattr(resources, "ngpus") and resources.ngpus > 0:
         select = (
             r"#PBS -lselect=1:ncpus="
-            + str(resources.ncpus)
-            + ":ompthreads="
             + str(resources.ncpus)
             + ":mem="
             + str(resources.mem)
@@ -52,12 +49,19 @@ def writePBSScript(name, resources, command):
 
     script += r"#PBS -N " + name + "\n"
     script += r"#PBS -V" + "\n"
-    script += r"cd $PBS_O_WORKDIR;" + "\n"
-    script += "time " + command
 
-    f = open(location, "w")
-    f.write(script)
-    f.close()
+    # Array job handling
+    if array_indices:
+        script += f"#PBS -J 0-{len(array_indices)-1}\n"
+        script += r"ARRAY_ID=$(printf %04d $PBS_ARRAYID)" + "\n"
+        script += r"cd $PBS_O_WORKDIR;" + "\n"
+        script += f"time {command}[$ARRAY_ID]" + "\n"
+    else:
+        script += r"cd $PBS_O_WORKDIR;" + "\n"
+        script += f"time {command}" + "\n"
+
+    with open(location, "w") as f:
+        f.write(script)
 
     return location
 
@@ -115,29 +119,66 @@ class Executor(RemoteExecutor):
     def __post_init__(self):
         self.workflow
         self.workflow.executor_settings
+        self.job_groups = defaultdict(list)
 
     def run_job(self, job: JobExecutorInterface):
-        # Prepare the job submission command
-        job_cmd = self.format_job_exec(job)
+        # Group jobs by rule name for array job submission
+        self.job_groups[job.rule.name].append(job)
 
+    def submit_jobs(self):
         home = os.path.expanduser("~")
 
         if not os.path.exists(home + "/logs"):
             os.makedirs(home + "/logs")
             os.makedirs(home + "/jobs")
 
+        for rule_name, jobs in self.job_groups.items():
+            if len(jobs) > 1:
+                # Create an array job
+                self.submit_array_job(jobs, rule_name, home)
+            else:
+                # Submit single job normally
+                self.submit_single_job(jobs[0], home)
+
+    def submit_array_job(self, jobs, rule_name, home):
+        job_array_index = 0
+        array_indices = []
+
+        for job in jobs:
+            name = f"{job.rule.name}.{job.jobid}"
+            env = subprocess.check_output("export -p", shell=True).decode("utf-8")
+            job_cmd = env + "\n" + self.format_job_exec(job)
+            bashLoc = writeBashScript(home + "/jobs", name, job_cmd)
+            array_indices.append(job_array_index)
+            job_array_index += 1
+
+        scriptLoc = writePBSScript(rule_name, jobs[0].resources, array_indices, bashLoc)
+
+        cmd = f"qsub -t 0-{len(array_indices)-1} {scriptLoc}"
+
+        try:
+            result = subprocess.run(
+                cmd, shell=True, check=True, capture_output=True, text=True
+            )
+            job_id = result.stdout.strip()
+            for job in jobs:
+                job_info = SubmittedJobInfo(
+                    job=job, external_jobid=f"{job_id}[{job_array_index}]"
+                )
+                self.report_job_submission(job_info)
+        except subprocess.CalledProcessError as e:
+            raise WorkflowError(f"Failed to submit array job: {e.stderr}")
+
+    def submit_single_job(self, job, home):
         name = f"{job.rule.name}.{job.jobid}"
-
         env = subprocess.check_output("export -p", shell=True).decode("utf-8")
-        job_cmd = env + "\n" + job_cmd
-
+        job_cmd = env + "\n" + self.format_job_exec(job)
         bashLoc = writeBashScript(home + "/jobs", name, job_cmd)
         scriptLoc = writePBSScript(name, job.resources, bashLoc)
 
         cmd = f"qsub {scriptLoc}"
 
         try:
-            # Submit the job to PBS
             result = subprocess.run(
                 cmd, shell=True, check=True, capture_output=True, text=True
             )
